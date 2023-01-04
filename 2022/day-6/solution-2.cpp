771b4f983b52d47813aa4b48e3a471e57059f5f2
Baljinder Benipal <bbenipal10@gmail.com>
2023-01-03 23:35:03 -0800

    Output characters to process for start-of-message

---
+++ 2022/day-6/solution-2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+  const int MARKER_LENGTH = 14;
+
+  ifstream file("input.txt");
+  string buffer = "";
+
+  getline(file, buffer);
+
+  int bufferIndex = 0;
+  int numUniqueCharacters = 0;
+  bool isMarkerInBuffer = false;
+
+  unordered_map<char, int> characterFrequencyMap;
+
+  while (bufferIndex < buffer.size()) {
+    if (bufferIndex >= MARKER_LENGTH) {
+      const char nonMarkerCharacter = buffer[
+        bufferIndex - MARKER_LENGTH
+      ];
+
+      --characterFrequencyMap[nonMarkerCharacter];
+
+      const int frequency = characterFrequencyMap[nonMarkerCharacter];
+
+      if (frequency == 0) {
+        --numUniqueCharacters;
+      } else if (frequency == 1) {
+        ++numUniqueCharacters;
+      }
+    }
+
+    const char currentCharacter = buffer[bufferIndex];
+    ++characterFrequencyMap[currentCharacter];
+
+    if (characterFrequencyMap[currentCharacter] == 1) {
+      ++numUniqueCharacters;
+    } else if (characterFrequencyMap[currentCharacter] == 2) {
+      --numUniqueCharacters;
+    }
+
+    if (numUniqueCharacters == MARKER_LENGTH) {
+      isMarkerInBuffer = true;
+      break;
+    }
+
+    ++bufferIndex;
+  }
+
+  if (!isMarkerInBuffer) {
+    cout << "No start of packet marker was found!" << endl;
+    return 0;
+  }
+
+  const int numCharactersProcessed = bufferIndex + 1;
+
+  cout
+    << "The number of characters to process to get the marker is: "
+    << numCharactersProcessed
+    << endl;
+
+  return 0;
+}
