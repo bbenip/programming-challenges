6f90d8fcc9c4dff0acb87ebe2555896b65855d65
Baljinder Benipal <bbenipal10@gmail.com>
2023-10-19 01:01:03 -0400

    Fix solution to LeetCode problem #219
    The original solution was incorrect. The solution compared the original
    indices of adjacent numbers after sorting them. However, as mentioned in
    the comments of the solution, the sort is not stable and, in theory, it
    is possible for a number to be inserted in between 2 equal numbers whose
    original indices are within k of each other, but whose original indices
    are not within k of the original index of the inserted element.
    Thereafter, iterating through the adjacent elements would errouneously
    return false.
    
    In addition, the runtime of the current solution is better than the
    original solution.
    
    Original runtime complexity: O(n*logn)
    Current runtime complexity: O(n)
    
    Where n is the size of the input array.

--- leet-code/00219-contains-duplicates-2.cpp
+++ leet-code/00219-contains-duplicates-2.cpp
@@ -1,34 +1,21 @@
 class Solution {
   public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-      vector<pair<int, int>> numbersAndIndices;
+      unordered_map<int, int> previousIndicesOfNumbers;
 
       for (int i = 0; i < nums.size(); ++i) {
-        const int currentNumber = nums[i];
-        numbersAndIndices.push_back({ currentNumber, i } );
-      }
+        const int number = nums[i];
 
-      sort(
-        numbersAndIndices.begin(),
-        numbersAndIndices.end(),
-        [] (const auto& numberAndIndex1, const auto& numberAndIndex2) {
-          const auto& [number1, _index1] = numberAndIndex1;
-          const auto& [number2, _index2] = numberAndIndex2;
+        const bool isWithinKElementsOfPreviousOccurence = (
+          previousIndicesOfNumbers.contains(number)
+          && i - previousIndicesOfNumbers[number] <= k
+        );
 
-          return number1 < number2;
-        }
-      );
-
-      for (int i = 1; i < numbersAndIndices.size(); ++i) {
-        const auto& [number1, index1] = numbersAndIndices[i - 1];
-        const auto& [number2, index2] = numbersAndIndices[i];
-
-        // Use abs() because sort is not guaranteed to be stable in C++
-        // Do not use stable_sort as its runtime can be polyloglinear
-        // i.e., O(n*(log(n))^2)
-        if (number1 == number2 && abs(index2 - index1) <= k) {
+        if (isWithinKElementsOfPreviousOccurence) {
           return true;
         }
+
+        previousIndicesOfNumbers[number] = i;
       }
 
       return false;
