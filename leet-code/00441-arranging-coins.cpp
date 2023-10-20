647284bdec5fdcba74b3d4568b99808c231fe0f3
Baljinder Benipal <bbenipal10@gmail.com>
2023-10-20 14:02:27 -0400

    Improve solution to LeetCode problem #441
    In our original approach, we determine the number of levels by
    iteratively going row by row to see if we had enough to coins to make
    that row. The number of rows we create is k where k is the largest value
    for which the sum from 1 to k (i.e., the sum of all coins in the k rows)
    is less than or equal to n. Based on our understanding of triangle
    numbers, such a value is equal to (or 1 less than) the value m such that
    n = (m * (m + 1)) / 2. Using the quadratic formula and substituting for
    k we get that k is O(sqrt(n)).
    
    On the other hand, this knowledge can also inform an improved approach.
    We can set bounds for the number of rows based on the maximum and
    minimum triangle numbers that fit within the constraints (i.e., 1 <= n
    <= 2^31 -1). Then, we can perform a binary search where we check, in
    constant time, with our formula for the the sum of integers 1 to m, the
    number of coins required to make m rows. Thereafter, we can update the
    bounds. Each iteration halves the search space until we find the
    appropriate number of rows.
    
    Technically, this algorithm can be said to run in constant time since we
    are dealing with fixed bounds and halving these bounds predictably.
    However, these bounds emerge from an understanding of the constraints.
    The upper bound comes from the largest possible value of our input n and
    is in proportion to its square root. Thus, we can argue that the runtime
    is Ω(log(sqrt(n))) for all values of n and Θ(log(sqrt(n)) for the
    largest value of n.

--- leet-code/00441-arranging-coins.cpp
+++ leet-code/00441-arranging-coins.cpp
@@ -1,13 +1,36 @@
 class Solution {
   public:
     int arrangeCoins(int n) {
-      int level = 1;
+      // These bounds are based on their respective triangle numbers
+      // The nth triangle number is the sum of all integers from 1 to n
+      // T_1 is 1
+      // T_65536 is the smallest triangle number greater than 2^31
+      int numRowsLowerBound = 1;
+      int numRowsUpperBound = 65536;
 
-      while (n >= level) {
-        n -= level;
-        level += 1;
+      // The lower bound is always less than the upper bound in this loop
+      // So, the triangle number calculated is less than T_65536
+      // As a result, no overflow should occur
+      while (numRowsLowerBound < numRowsUpperBound) {
+        // Calculate midpoint between bounds without overflow
+        const int numRows = (
+          numRowsLowerBound + (numRowsUpperBound - numRowsLowerBound) / 2
+        );
+
+        // Calculate the triangle number without overflow
+        const bool isNumRowsEven = numRows % 2 == 0;
+        const int factor1 = isNumRowsEven ? numRows / 2: numRows;
+        const int factor2 = isNumRowsEven ? numRows + 1 : (numRows + 1) / 2;
+
+        const int numberOfCoinsRequired = factor1 * factor2;
+
+        if (numberOfCoinsRequired > n) {
+          numRowsUpperBound = numRows;
+        } else {
+          numRowsLowerBound = numRows + 1;
+        }
       }
 
-      return level - 1;
+      return numRowsUpperBound - 1;
     }
 };
