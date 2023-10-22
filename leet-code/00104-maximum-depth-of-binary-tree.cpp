17d2378c96f2b822209d74432b5a997f26ac4944
Baljinder Benipal <bbenipal10@gmail.com>
2023-10-22 13:24:53 -0400

    Solve LeetCode problem #104
    Given the root of a binary tree, return its maximum depth.

---
+++ leet-code/00104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+  public:
+    int maxDepth(TreeNode* root) {
+      if (root == nullptr) {
+        return 0;
+      }
+
+      const int maxLeftDepth = 1 + maxDepth(root->left);
+      const int maxRightDepth = 1 + maxDepth(root->right);
+
+      return max(maxLeftDepth, maxRightDepth);
+    }
+};
