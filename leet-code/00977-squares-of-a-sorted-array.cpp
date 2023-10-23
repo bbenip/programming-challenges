1295a7193d20f17b1bd7ffe21f49cde521c97d55
Baljinder Benipal <bbenipal10@gmail.com>
2023-10-23 19:06:03 -0400

    Update solution to LeetCode problem #977
    The original approach involved searching for the first non-negative
    value's index and then reversing the list of negative numbers on the
    left of this index. This effectively sorted it into ascending order of
    absolute value. Then, two pointers were used to compare the smallest (in
    absolute value) negative value with the smallest non-negative value
    until all values of the original array had been collated into a new
    array in order of absolute value. Finally, a last pass-through was done
    to square all the numbers in the new array of collated values.
    
    The current approach initializes the output array with the correct size
    (i.e., the same size as the input array). Then, the output is built
    backwards (i.e., from the largest squared value to the smallest). To do
    this, two pointers are used to track the smallest and largest values.
    One of the smallest and largest values is guaranteed to have the largest
    absolute value of all the values in array. The output is generated by
    squaring that value and adding it to the back of the output array. The
    indices for the smallest and largest value are updated accordingly.
    
    Original Runtime Complexity: O(n)
    Current Runtime Complexity: O(n)
    
    Original Space Complexity: O(n)
    Current Space Complexity: O(n)

--- leet-code/00977-squares-of-a-sorted-array.cpp
+++ leet-code/00977-squares-of-a-sorted-array.cpp
@@ -1,63 +1,24 @@
 class Solution {
   public:
     vector<int> sortedSquares(vector<int>& nums) {
-      vector<int> squaredNums;
+      vector<int> squaredNums(nums.size());
 
-      // Find first non-negative number
-      int lowerBound = 0;
-      int upperBound = nums.size();
+      int smallestValueIndex = 0;
+      int largestValueIndex = nums.size() - 1;
 
-      while (lowerBound < upperBound) {
-        // Calculate midpoint index without overflow
-        const int candidateIndex = lowerBound + (upperBound - lowerBound) / 2;
-        const int candidate = nums[candidateIndex];
+      while (smallestValueIndex <= largestValueIndex) {
+        const int smallestValue = nums[smallestValueIndex];
+        const int largestValue = nums[largestValueIndex];
 
-        if (candidate < 0) {
-          lowerBound = candidateIndex + 1;
+        const int currentIndex = largestValueIndex - smallestValueIndex;
+
+        if (abs(smallestValue) > largestValue) {
+          squaredNums[currentIndex] = smallestValue * smallestValue;
+          smallestValueIndex += 1;
         } else {
-          upperBound = candidateIndex;
+          squaredNums[currentIndex] = largestValue * largestValue;
+          largestValueIndex -= 1;
         }
-      }
-
-      // Sort negative numbers in ascending order of absolute value
-      reverse(nums.begin(), nums.begin() + lowerBound);
-
-      // Collate negative and non-negative numbers based on absolute value
-      int negativeSubarrayIndex = 0;
-      int nonNegativeSubarrayIndex = lowerBound;
-
-      while (
-        negativeSubarrayIndex < lowerBound
-        && nonNegativeSubarrayIndex < nums.size()
-      ) {
-        const int negativeValue = nums[negativeSubarrayIndex];
-        const int nonNegativeValue = nums[nonNegativeSubarrayIndex];
-
-        if (abs(negativeValue) < nonNegativeValue) {
-          squaredNums.push_back(negativeValue);
-          negativeSubarrayIndex += 1;
-        } else {
-          squaredNums.push_back(nonNegativeValue);
-          nonNegativeSubarrayIndex += 1;
-        }
-      }
-
-      // Insert remaining portion of subarrays
-      squaredNums.insert(
-        squaredNums.end(),
-        nums.begin() + negativeSubarrayIndex,
-        nums.begin() + lowerBound
-      );
-
-      squaredNums.insert(
-        squaredNums.end(),
-        nums.begin() + nonNegativeSubarrayIndex,
-        nums.end()
-      );
-
-      // Square every element, now in the correct order
-      for (auto& num : squaredNums) {
-        num *= num;
       }
 
       return squaredNums;
