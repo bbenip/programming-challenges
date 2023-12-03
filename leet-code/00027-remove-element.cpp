4b8a67afd602d83b7d14eac9a61d5d7b29fa841e
Baljinder Benipal <bbenipal10@gmail.com>
2023-12-03 14:54:24 -0500

    Update solution to LeetCode problem #27
    The original approach starts from the end of the array and swaps the
    values to be removed to the end. On the other hand, the current approach
    starts at the beginning of the array and adds valid elements as it goes.
    
    The benefit of the current approach is that it maintains the relative
    order of elements from the original array.
    
    Original Runtime Complexity: O(n)
    Original Space Complexity: O(1)
    
    Current Runtime Complexity: O(n)
    Current Space Complexity: O(1)

--- leet-code/00027-remove-element.cpp
+++ leet-code/00027-remove-element.cpp
@@ -1,26 +1,16 @@
 class Solution {
   public:
     int removeElement(vector<int>& nums, int val) {
-      int numberOfRemovedElements = 0;
+      int numberOfRemainingElements = 0;
 
-      for (int i = nums.size() - 1; i >= 0; --i) {
-        int& currentNumber = nums[i];
+      for (int i = 0; i < nums.size(); ++i) {
+        const int& currentNumber = nums[i];
 
-        if (currentNumber == val) {
-          // This element can only be val if all the elements after are also val
-          // In this case, the following procedure is still correct
-          int& lastNonValNumber = nums[
-            nums.size() - 1 - numberOfRemovedElements
-          ];
-
-          swap(currentNumber, lastNonValNumber);
-          numberOfRemovedElements += 1;
+        if (currentNumber != val) {
+          nums[numberOfRemainingElements] = currentNumber;
+          numberOfRemainingElements += 1;
         }
       }
-
-      const int numberOfRemainingElements = (
-        nums.size() - numberOfRemovedElements
-      );
 
       return numberOfRemainingElements;
     }
