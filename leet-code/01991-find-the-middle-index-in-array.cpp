5cf9f1b59d553b254102291ec09db655aa79f180
Baljinder Benipal <bbenipal10@gmail.com>
2023-12-06 17:14:26 -0500

    Solve LeetCode problem #1991
    Given an integer array, return the leftmost middle index, if it exists;
    otherwise, return -1.
    
    A middle index is an index where the sum of all elements to the left of
    the index is equal to the sum of all elements to the right of the index.

---
+++ leet-code/01991-find-the-middle-index-in-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int findMiddleIndex(vector<int>& nums) {
+      int leftSum = 0;
+      int inclusiveRightSum = 0;
+
+      for (const auto& num : nums) {
+        inclusiveRightSum += num;
+      }
+
+      for (int i = 0; i < nums.size(); ++i) {
+        const int rightSum = inclusiveRightSum - nums[i];
+
+        if (leftSum == rightSum) {
+          return i;
+        }
+
+        leftSum += nums[i];
+        inclusiveRightSum = rightSum;
+      }
+
+      return -1;
+    }
+};
