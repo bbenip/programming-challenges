6f82b020d3895df6cc70d08885fd29d5bf4d070a
Baljinder Benipal <bbenipal10@gmail.com>
2023-12-23 14:32:28 -0500

    Solve LeetCode problem #2621
    Given a positive integer millis, write an asynchronous function that sleeps for
    millis milliseconds.

---
+++ leet-code/02621-sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+const sleep = async (millis) => (
+  new Promise((resolve) => setTimeout(resolve, millis))
+);
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
