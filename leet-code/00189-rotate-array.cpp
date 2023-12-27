5b8f2f6c79fc4cc669dbcae2bab4d46f20d26e36
Baljinder Benipal <bbenipal10@gmail.com>
2023-12-27 14:28:14 -0500

    Update solution to LeetCode problem #189
    The original solution uses fast and slow pointers to detect cycles and
    rotates the array 1 modulo class at a time until all elements have been
    rotated.
    
    The current solution updates k to be the smallest equivalent
    non-negative value possible; any number of rotations which is a multiple
    of the size of the array has no effect. Next, the solution reverses the
    entire array, reverses the first k elements, and finally reverses the
    remaining elements separately.
    
    Original Runtime Complexity: O(n)
    Original Space Complexity: O(1)
    
    Current Runtime Complexity: O(n)
    Current Space Complexity: O(1)

--- leet-code/00189-rotate-array.cpp
+++ leet-code/00189-rotate-array.cpp
@@ -1,38 +1,10 @@
 class Solution {
   public:
     void rotate(vector<int>& nums, int k) {
-      const bool isRotatedArrayIdentical = k % nums.size() == 0;
+      k %= nums.size();
 
-      if (isRotatedArrayIdentical) {
-        return;
-      }
-
-      int elementsRotated = 0;
-
-      for (int i = 0; i < nums.size(); ++i) {
-        if (elementsRotated == nums.size()) {
-          return;
-        }
-
-        int numberInTransit = nums[i];
-
-        int slowIndex = (i + k) % nums.size();
-        int fastIndex = (i + (2 * k)) % nums.size();
-
-        while (slowIndex != fastIndex) {
-          const int nextNumberInTransit = nums[slowIndex];
-
-          nums[slowIndex] = numberInTransit;
-          elementsRotated += 1;
-
-          numberInTransit = nextNumberInTransit;
-
-          slowIndex = (slowIndex + k) % nums.size();
-          fastIndex = (fastIndex + (2 * k)) % nums.size();
-        }
-
-        nums[slowIndex] = numberInTransit;
-        elementsRotated += 1;
-      }
+      reverse(nums.begin(), nums.end());
+      reverse(nums.begin(), nums.begin() + k);
+      reverse(nums.begin() + k, nums.end());
     }
 };
